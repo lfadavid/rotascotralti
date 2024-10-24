8384d7324e51502285d530189788ca2f08f0a2e9
lfadavid <167301084+lfadavid@users.noreply.github.com>
2024-10-24 05:54:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ hide_github_icon = """
 }
 """
 
-
-
 def hide_anchor_link():
     st.markdown(
         body="""
@@ -71,6 +69,7 @@ st.header("Consulta por Cidade!", divider='gray')
 
 DataFrame = pd.read_excel("Base Rotas.xlsx", index_col=4)#index_col=2
 DataFrame["Cidade"] = DataFrame["Cidade"].astype("string")
+DataFrame["Tabela de Fat."] = DataFrame["Tabela de Fat."].astype("string")
 DataFrame["Estado de Destino"]  = DataFrame["Estado de Destino"]
 
 with open ('style.css') as f:
@@ -84,8 +83,6 @@ st.sidebar.markdown("""
 
 st.sidebar.header("Filtro")
 
-
-
 cidade = st.sidebar.selectbox(
                 key=1,
                 label="Cidade",
@@ -95,18 +92,30 @@ cidade = st.sidebar.selectbox(
                 index=None
                 
 )
+
+tabela_de_fat = st.sidebar.multiselect(
+                key=2,
+                label="Tabela de Fat.",
+                options=DataFrame["Tabela de Fat."].unique(),
+                help="Selecione a rota e click nela."
+                
+            )
 if cidade:
     if len(cidade) != None:
         DataFrame = DataFrame.query("Cidade== @cidade")
         st.success("Encontramos a cidade selecionada!")
+
+elif tabela_de_fat:
+    if len(tabela_de_fat) != None:
+        DataFrame = DataFrame.query(" `Tabela de Fat.`== @tabela_de_fat")
+        st.success("Encontramos a rota selecionada!")  
        
 colunas_aparecer = ["Tabela de Fat.", "Saída Spice","Cidade", "Região", "Dias de Entrega", "Lead Time", "Habitantes", "Imagem"]
 st.dataframe(DataFrame[colunas_aparecer],use_container_width=True,
-            column_config={ "Lead Time": st.column_config.ProgressColumn("Tempo de Entrega",format="D+%d", min_value=1, max_value=5),
-                            "Imagem": st.column_config.ImageColumn("Cartão Postal", help="ok"),
-                            "Habitantes":st.column_config.NumberColumn(step=".01"),
-            })
-
+                column_config={ "Lead Time": st.column_config.ProgressColumn("Tempo de Entrega",format="D+%d", min_value=1, max_value=5),
+                                "Imagem": st.column_config.ImageColumn("Cartão Postal", help="ok"),
+                                "Habitantes":st.column_config.NumberColumn(step=".01"),
+                })
 st.write("""
          &copy; 2024 - Luis Felipe A. David. Todos os direitos reservados
          """)
